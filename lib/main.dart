12c896f2bd8b36d58515a17d197921a79ab90cfa
DANIELA_LOPEZ_CHAVIRA <a.22308051281240@cbtis128.edu.mx>
2025-05-09 03:39:03 +0000

    terminado

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,205 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:lopezact8/ViewBolsas.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 320,
+              width: double.infinity,
+              padding: EdgeInsets.only(left: 25, right: 25, top: 60),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(50.0),
+                    bottomLeft: Radius.circular(50.0)),
+                gradient: LinearGradient(begin: Alignment.topRight, colors: [
+                  Color.fromRGBO(255, 250, 182, 1),
+                  Color.fromRGBO(255, 239, 78, 1)
+                ]),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/menu.png',
+                    width: 20,
+                  ),
+                  SizedBox(height: 15),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 4,
+                        child: Text('La mejor coleccion de \nbolsas, en linea',
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromRGBO(97, 90, 90, 1))),
+                      ),
+                      Expanded(
+                        flex: 8,
+                        child: Image.asset('assets/bolsa.png'),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Transform.translate(
+              offset: Offset(0, -35),
+              child: Container(
+                height: 60,
+                padding: EdgeInsets.only(left: 20, top: 8),
+                margin: EdgeInsets.symmetric(horizontal: 25),
+                decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.shade300,
+                          blurRadius: 20.0,
+                          offset: Offset(0, 10.0))
+                    ],
+                    borderRadius: BorderRadius.circular(5.0),
+                    color: Colors.white),
+                child: TextField(
+                  decoration: InputDecoration(
+                      suffixIcon: Icon(
+                        Icons.search,
+                        color: Colors.black,
+                        size: 20.0,
+                      ),
+                      border: InputBorder.none,
+                      hintText: 'Search'),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(25),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text('Elige \ntu categoria',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Color.fromRGBO(97, 90, 90, 1))),
+                      Container(
+                        child: Row(
+                          children: <Widget>[
+                            MaterialButton(
+                              elevation: 0,
+                              highlightElevation: 0,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5)),
+                              padding: EdgeInsets.all(10),
+                              onPressed: () {},
+                              color: Color.fromRGBO(251, 53, 105, 0.1),
+                              child: Text('Mujer',
+                                  style: TextStyle(
+                                      color: Color.fromRGBO(251, 53, 105, 1),
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                            SizedBox(width: 20.0),
+                            MaterialButton(
+                              elevation: 0,
+                              highlightElevation: 0,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5)),
+                              padding: EdgeInsets.all(10),
+                              onPressed: () {},
+                              color: Color.fromRGBO(97, 90, 90, 0.1),
+                              child: Text('Hombre',
+                                  style: TextStyle(
+                                      color: Color.fromRGBO(97, 90, 90, 0.6),
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Container(
+              height: 280,
+              width: double.infinity,
+              child: ListView(
+                padding: EdgeInsets.only(bottom: 20, left: 20),
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  makeCard(
+                      context: context,
+                      startColor: Color.fromRGBO(251, 121, 155, 1),
+                      endColor: Color.fromRGBO(251, 53, 105, 1),
+                      image: 'assets/bolsa2.png'),
+                  makeCard(
+                      context: context,
+                      startColor: Color.fromRGBO(203, 251, 255, 1),
+                      endColor: Color.fromRGBO(81, 223, 234, 1),
+                      image: 'assets/bolsa3.png'),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    );
+  }
+
+  Widget makeCard(
+      {required BuildContext context,
+      required Color startColor,
+      required Color endColor,
+      required String image}) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ViewBolsas()),
+        );
+      },
+      child: AspectRatio(
+        aspectRatio: 4 / 5,
+        child: Container(
+          margin: EdgeInsets.only(right: 20),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(13.0),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                colors: [startColor, endColor],
+              ),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.shade300,
+                    blurRadius: 10,
+                    offset: Offset(5, 10))
+              ]),
+          child: Padding(
+            padding: EdgeInsets.all(50),
+            child: Center(
+              child: Image.asset(image),
+            ),
+          ),
         ),
       ),
     );
